19e40ac6b12f2f75898dcb3587e7f9935ee37632
Roberto Cornacchiari <roberto.cornacchiari@virgilio.it>
2023-02-09 21:59:39 +0100

    Setup pie graph

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Graph from "./Components/Graph";
@@ -7,19 +7,51 @@ import Table from "./Components/Table";
 import "./index.css";
 import { ITraderGood } from "./interfaces";
 
+import { Pie } from "@ant-design/plots";
+
 const App = () => {
   const [data, setData] = useState<ITraderGood[]>([]);
+
+  const [lastData, setLastData] = useState<Map<string, number>>(new Map());
+
   useEffect(() => {
     let connection = new EventSource("/currentTraderGoods");
     connection.addEventListener("message", (ev) => {
-      const msg = JSON.parse(ev.data);
+      const msg: ITraderGood = JSON.parse(ev.data);
       console.log(msg);
       setData((prev) => [msg, ...prev]);
+      setLastData(new Map(lastData.set(msg.kind, msg.quantity)));
     });
     return () => {
       connection.close();
     };
   }, []);
+
+  const config = useMemo(() => {
+    return {
+      appendPadding: 10,
+      data: Array.from(lastData, ([key, value]) => ({
+        kind: key,
+        quantity: value,
+      })),
+      angleField: "quantity",
+      colorField: "kind",
+      radius: 0.8,
+      label: {
+        type: "outer",
+        content: "{name} {percentage}",
+      },
+      interactions: [
+        {
+          type: "pie-legend-active",
+        },
+        {
+          type: "element-active",
+        },
+      ],
+    };
+  }, [lastData]);
+
   return (
     <Routes>
       <Route
@@ -49,6 +81,8 @@ const App = () => {
                 yField="quantity"
                 seriesField="kind"
               />
+              <br />
+              <Pie {...config} />
             </div>{" "}
             <a href="/marketController">
               <button>Go to Markets!</button>
